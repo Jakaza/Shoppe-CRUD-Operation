be0a351e54ab6655d804c34580031507d3697ad4
Themba G <goodnessthembac@gmail.com>
2022-09-11 08:46:59 +0200

    add ProductConstants class, contain values for database attributes

---
+++ src/crud/operation/sql/ProductConstants.java
@@ -0,0 +1,16 @@
+package crud.operation.sql;
+
+/**
+ *
+ * @author Jakaza G Chauke
+ */
+public interface ProductConstants {
+    public static String tableName = "ProductTBL";
+    
+    public static String COLUMN_NAME = "Product_Name";
+    public static String COLUMN_DESCRIPTION = "Product_Name";
+    public static String COLUMN_CODE = "Product_Code";
+    public static String COLUMN_ID = "Product_ID";
+    public static String COLUMN_STATUS = "Product_Status";
+   
+}
