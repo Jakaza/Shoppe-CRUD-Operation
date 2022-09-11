a2993ee800d6579eb0b22900745388f15dfacd0d
Themba G <goodnessthembac@gmail.com>
2022-09-11 08:59:56 +0200

    Add ProductManagement Interface, define all the methods to manage the products

---
+++ src/crud/operation/backend/ProductManagement.java
@@ -0,0 +1,18 @@
+package crud.operation.backend;
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ *
+ * @author Jakaza G Chauke
+ */
+public interface ProductManagement {
+    
+    public void addProduct(String sql, Product product)throws SQLException;
+    public void deleteProduct(String sql, Integer product_id)throws SQLException;
+    public void updateProduct(String sql, Integer product_id) throws SQLException;
+    public void updateProduct(String sql, String product_code) throws SQLException;
+    public List<Product> getAllProduct()throws SQLException;
+    public Product getProduct(String sql, Integer product_id)throws SQLException;
+    
+}
